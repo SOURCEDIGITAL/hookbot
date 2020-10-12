d9567ced6641a035c3d8fab445135b25b284abd5
reganlawton <reganlawton92@gmail.com>
2020-10-13 09:13:09 +1100

    Remove buddybuild

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const request = require("request");
 const bugsnagWebhook = process.env["BUGSNAG_WEBHOOK"];
-const buddybuildWebhook = process.env["BUDDYBUILD_WEBHOOK"];
 
 function assertEnvVariable(variable) {
     if (!process.env[variable]) {
@@ -13,7 +12,6 @@ function assertEnvVariable(variable) {
 }
 
 assertEnvVariable("BUGSNAG_WEBHOOK");
-assertEnvVariable("BUDDYBUILD_WEBHOOK")
 
 app.use(bodyParser.json());
 
@@ -70,91 +68,7 @@ if (bugsnagWebhook) {
     app.post('/webhooks/bugsnag', bugsnagHandler);
 }
 
-// Hipchat supports: yellow, green, red, purple, gray, random.
-function convertHipchatColorToDiscord(color) {
-    const COLORS = {
-        yellow: 0xF1C40F,
-        green: 0x2ECC71,
-        red: 0xE74C3C,
-        purple: 0x9B59B6,
-        gray: 0x95A5A6,
-        grey: 0x95A5A6
-    }
-    if (color.toLowerCase() === 'random') {
-        return Math.floor(Math.random() * (0xFFFFFF + 1));
-    }
-    const resolvedColor = COLORS[color.toLowerCase()];
-    if (resolvedColor) {
-        return resolvedColor;
-    } else {
-        throw new Error("invalid hipchat color encountered")
-    }
-}
-
-function convertHipchatAttrsToDiscordFields(attributes) {
-    let fields = [];
-    let actions = [];
-    for (const attr of attributes) {
-        let field = {};
-        if (!attr.label && attr.value.label && attr.value.url) {
-            actions.push(attr);
-            continue;
-        }
-        field.name = attr.label;
-        if (attr.value.url) {
-            field.value = `[${attr.value.label}](${attr.value.url})`;
-        } else {
-            field.value = attr.value.label;
-        }
-        fields.push(field);
-    }
-    if (actions.length > 0) {
-        let field = {};
-        field.name = "Actions";
-        field.value = actions.map((attr) => `[${attr.value.label}](${attr.value.url})`).join(" ");
-        fields.push(field);
-    }
-    return fields;
-}
-
-// Buddybuild
-function buddybuildHandler(req, res) {
-    const msg = req.body;
-    try {
-        const color = convertHipchatColorToDiscord(msg.color.toLowerCase());
-        const message = {
-            embeds: [
-                {
-                    title: msg.card.title,
-                    url: msg.card.url,
-                    description: msg.card.description,
-                    color: color,
-                    fields: convertHipchatAttrsToDiscordFields(msg.card.attributes)
-                }
-            ]
-        }
-
-        request({
-            url: buddybuildWebhook,
-            method: "POST",
-            body: message,
-            json: true
-        }, (e,r,b) => {
-            if (e) throw e;
-        });
-
-        res.status(200);
-        res.end();
-    } catch (error) {
-        console.error(error);
-        res.status(500);
-        res.end();
-    }
-}
-
-if (buddybuildWebhook) {
-    app.all('/webhooks/buddybuild', buddybuildHandler);
-}
+app.post('/webhooks/bugsnag', bugsnagHandler);
 
 // Default
 
